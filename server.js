1e4be0f83af63248e479c4b294e4288b1790c573
Zhuo Chen <chen519@usc.edu>
2018-05-24 18:40:06 -0700

    http server

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
-import config, {nodeEnv, logStars} from './config';
+/*
+// as a client
+import https from 'https';
+https.get('https://www.lynda.com', res => {
+  console.log('Response status code: ', res.statusCode);
 
-console.log(config, nodeEnv);
-logStars('Hello express!');
+  res.on('data', chunk => {
+    console.log(chunk.toString());
+  });
+});
+*/
+
+// as a server
+import http from 'http';
+const server = http.createServer((req, res) => {
+  res.write('Hello HTTP\n');
+  setTimeout(() => {
+    res.write('I can stream!\n');
+    res.end();
+  }, 3000);
+});
+
+server.listen(8080);
+
+//server.on('request', );
