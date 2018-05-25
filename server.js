aeccd761dda0d130378ec8180fd0b38af0e9d1a1
Zhuo Chen <chen519@usc.edu>
2018-05-24 23:50:20 -0700

    losass

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import config from './config';
-//import fs from 'fs';
 import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 const server = express();
+
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
 
 server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
-  //res.send('Hello Express\n');
   res.render('index', {
     content: 'Hello Express and <em>EJS</em>!'
   });
@@ -16,12 +21,7 @@ server.get('/', (req, res) => {
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
-// server.get('/about', (req, res) => {
-//   //res.send('The about page\n');
-//   fs.readFile('./about.html', (err, data) => {
-//     res.send(data.toString());
-//   });
-// });
+
 
 server.listen(config.port, () => {
   console.info('Express listening on port ', config.port);
